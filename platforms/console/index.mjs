9b50a471a53c1a357a4f761952835ea0e46f3b7b
Fulano de Tal <raziel_libertino@hotmail.com>
2021-03-18 22:31:00 -0400

    console view

--- platforms/console/index.mjs
+++ platforms/console/index.mjs
@@ -1,5 +1,33 @@
+import chalk from 'chalk';
+import chalkTable from 'chalk-table';
+
 export default class TableConsoleComponent {
     render(data) {
-        console.log("hi console!")
+        const columns = this.prepareData(data)
+
+        const options = {
+            leftPad: 2,
+            columns
+        }
+
+        const table = chalkTable(options, data);
+
+        console.log(table);
+    }
+
+    prepareData(data) {
+        const [ firstItem ] = data
+        const headers = Object.keys(firstItem);
+
+        const formatHeader = (data, index) => index % 2 === 0 ? 
+            chalk.yellow(data) : 
+            chalk.green(data)
+        
+        const columns = headers.map((item, index) => ({
+            field: item,
+            name: formatHeader(item, index)
+        }))
+
+        return columns;
     }
 }
